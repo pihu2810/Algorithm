5f3831789cc8af95cdb3f5b0ff0ccee2c148631f
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-11 14:30:28 +0530

    add MetgeSort

---
+++ src/com/Bridglabz/MergeSort.java
@@ -0,0 +1,64 @@
+package com.Bridglabz;
+
+import java.util.Arrays;
+
+public class MergeSort
+{
+	public static void main(String[] args) {
+        String[] values = {"of", "to", "welcome", "BridgLabz", "Number", "186"};
+        mergeSort(values, 0, values.length - 1);
+        System.out.println("Result " + Arrays.toString(values));
+    }
+
+    public static void mergeSort(String[] a, int from, int to) {
+        if (from == to) {
+            return;
+        }
+        int mid = (from + to) / 2;
+       
+        mergeSort(a, from, mid);
+        mergeSort(a, mid + 1, to);
+        merge(a, from, mid, to);
+    }
+
+    public static void merge(String[] a, int from, int mid, int to) {
+        int n = to - from + 1;       
+        String[] b = new String[n];   
+        int i1 = from;               
+        int i2 = mid + 1;            
+        int j = 0;                  
+
+        
+        while (i1 <= mid && i2 <= to) {
+            if (a[i1].compareTo(a[i2]) < 0) {
+                b[j] = a[i1];
+                i1++;
+            } else {
+                b[j] = a[i2];
+                i2++;
+            }
+            j++;
+        }
+
+    
+        while (i1 <= mid) {
+            b[j] = a[i1];
+            i1++;
+            j++;
+        }
+
+       
+        while (i2 <= to) {
+            b[j] = a[i2];
+            i2++;
+            j++;
+        }
+
+        
+        for (j = 0; j < n; j++) {
+            a[from + j] = b[j];
+        }
+    }
+
+}
+
